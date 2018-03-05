03c9d96c8d1e26da8d868b640e154107817495ab
ArnoGranier <arno.granier@etu.u-bordeaux.fr>
2018-03-05 08:19:29 +0100

    Delete __init__.py

--- TER - Copie/snn/single/__init__.py
+++
@@ -1,8 +0,0 @@
-from .core import *
-from .usual_models import *
-
-v = Variable(name='v', ddt='0.04*v**2+5*v+140-u+I', init_value=-65, 
-			 reset_value='c', unit='mV')
-u = Variable(name='u', ddt='a*(b*v-u)', init_value=-15, reset_value='u+d')
-izhi_model = Model(v, u, spike_when='v>=30', max_spike_value=30, 
-        a=0.02, b=0.2, c=-65, d=8, I=lambda t : 0 if t<200 or t>800 else 5)
