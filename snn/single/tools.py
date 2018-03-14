067003dffc3f92e1037da0a1cf73e353003ec9e3
ArnoGranier <arno.granier@etu.u-bordeaux.fr>
2018-03-14 19:51:22 +0100

    Update tools.py

--- snn/single/tools.py
+++ snn/single/tools.py
@@ -8,19 +8,6 @@ class Callable_Float(float):
         return float.__new__(self, value)
     def __call__(self, *args, **kwargs): return self
 
-class Function:
-    def __init__(self, arguments, foo):
-        if arguments is None: self.arguments = ['t', ]
-        else: 
-            try :
-                iter(arguments)
-                self.arguments = arguments
-            except: self.arguments = [arguments, ]
-        if callable(foo) : self.foo = foo
-        else: self.foo = eval('lambda %s : %s' % (','.join(arguments), foo)) 
-
-    def __call__(self, *args) : return self.foo(*args)
-
 def create_equation(expected_args, eq):
     str_expected = ','.join('%s=None' % arg for arg in expected_args)
     return eval('lambda %s:%s' % (str_expected, eq)) if eq is not None else None
